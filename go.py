be8f3637afdfe9111989180b53b17c216e78de58
guiker <guiker@vip.qq.com>
2018-07-23 20:16:24 +0800

    up
    up

---
+++ go.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#===================================
+# @Version:
+# @Author:guiker
+# @Create Time:00:39 01/05/2018
+# @File Name:go.py
+# @Description:
+#
+#===================================
+import flyCat
+paw = flyCat.Paw()
+paw.start()
