bf689746bc3774383f704c115654f5a8b3345e6d
guiker <guiker@vip.qq.com>
2018-07-25 23:26:30 +0800

    V0.1 Beta
    V0.1 Beta

---
+++ flyCat/__version__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#===================================
+# -*- coding: utf-8 -*-
+
+__title__ = 'flyCat'
+__description__ = 'IP Proxy Poll'
+__url__ = 'https://github.com/guiker/flyCat'
+__version__ = '0.1 Beta'
+__author__ = 'Guiker'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018 Guiker'
